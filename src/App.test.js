dff7fce76d6f6abc25a83013490d9443b663a3f5
DylanJo2002 <81334516+DylanJo2002@users.noreply.github.com>
2023-10-29 00:12:58 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('ENERGYM POINT');
+  const linkElement = screen.getByText('E31re4gw2ecc');
   expect(linkElement).toBeInTheDocument();
 });
